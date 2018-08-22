53ed8459d67d99467e044226bff9346e16c21c3b
tuttoDev2 <contato.eric.sm@gmail.com>
2018-08-22 10:56:55 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/ericsoares/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.ericsoares.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ericsoares.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{		
+
+}
